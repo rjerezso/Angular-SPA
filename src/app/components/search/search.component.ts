078fab2e908e15d44e9fc1c34cf3034d0f699a83
Rodri.J <rjerezso@gmail.com>
2021-03-05 21:01:57 +0100

    buscador completo

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-search',
@@ -7,12 +8,19 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class SearchComponent implements OnInit {
 
-  constructor( private activatedRoute:ActivatedRoute) { }
+  heroes:any[] = [];
+  termino:string = "";
+  
+
+  constructor( private activatedRoute:ActivatedRoute,
+               private _heroesService: HeroesService
+    ) { }
 
   ngOnInit(): void {
     this.activatedRoute.params.subscribe( params =>{
-      
-    })
+      this.termino = params['termino'];
+      this.heroes = this._heroesService.buscarHeroes( params['termino']);
+    });
   }
 
 }
