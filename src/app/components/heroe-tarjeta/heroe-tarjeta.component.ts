d02d6601fde33f429045d587fd9e32b02331057d
Rodri.J <rjerezso@gmail.com>
2021-03-06 09:26:10 +0100

    heroe-tarjeta

--- src/app/components/heroe-tarjeta/heroe-tarjeta.component.ts
+++ src/app/components/heroe-tarjeta/heroe-tarjeta.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-heroe-tarjeta',
   templateUrl: './heroe-tarjeta.component.html',
-  styleUrls: ['./heroe-tarjeta.component.scss']
 })
 export class HeroeTarjetaComponent implements OnInit {
 
-  constructor() { }
+  @Input() heroe:any = {};
+  @Input() index!: number;
+
+  constructor( private _router:Router) { }
 
   ngOnInit(): void {
   }
 
+  verHeroe() {
+    this._router.navigate(['/heroe',this.index])
+  }
 }
